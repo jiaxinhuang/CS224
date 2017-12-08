ce62601a4a021449595afd263e4866401d4c33c2
jiaxinhuang <924785452@qq.com>
2017-12-08 16:59:15 +0800

    add q1_softmax

--- assignment2/q1_softmax.py
+++ assignment2/q1_softmax.py
@@ -24,6 +24,8 @@ def softmax(x):
     """
 
     ### YOUR CODE HERE
+    x_exp = tf.exp(x)
+    out = x_exp / tf.reduce_sum(x_exp, 1, keep_dims=True)
     ### END YOUR CODE
 
     return out
@@ -54,6 +56,8 @@ def cross_entropy_loss(y, yhat):
     """
 
     ### YOUR CODE HERE
+    y = tf.to_float(y)
+    out = tf.reduce_sum(-y * tf.log(yhat))
     ### END YOUR CODE
 
     return out
